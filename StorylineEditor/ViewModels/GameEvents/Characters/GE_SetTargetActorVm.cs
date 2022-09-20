b4baf7ff46b9d6202013cd2e619493e8caa1eeee
klauth86 <kehel@yandex.ru>
2022-09-20 13:55:07 +0300

    Fix Character events

--- StorylineEditor/ViewModels/GameEvents/Characters/GE_SetTargetActorVm.cs
+++ StorylineEditor/ViewModels/GameEvents/Characters/GE_SetTargetActorVm.cs
@@ -14,6 +14,7 @@ using StorylineEditor.Common;
 using StorylineEditor.ViewModels.Nodes;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 using System.Xml.Serialization;
 
 namespace StorylineEditor.ViewModels.GameEvents
@@ -74,6 +75,42 @@ namespace StorylineEditor.ViewModels.GameEvents
                 if (value != interactionType) {
                     interactionType = value;
                     NotifyWithCallerPropName();
+                }
+            }
+        }
+
+        protected CollectionViewSource actualCharactersSource;
+        [XmlIgnore]
+        public ICollectionView ActualCharacters
+        {
+            get
+            {
+                if (actualCharactersSource == null)
+                {
+                    actualCharactersSource = new CollectionViewSource() { Source = Parent?.Parent?.Parent?.Parent?.Characters };
+                }
+
+                if (actualCharactersSource.View != null)
+                {
+                    actualCharactersSource.View.Filter = (object obj) => string.IsNullOrEmpty(characterFilter) || obj != null && ((BaseVm)obj).PassFilter(characterFilter);
+                    actualCharactersSource.View.MoveCurrentTo(null);
+                }
+
+                return actualCharactersSource.View;
+            }
+        }
+
+        protected string characterFilter;
+        [XmlIgnore]
+        public string CharacterFilter
+        {
+            get => characterFilter;
+            set
+            {
+                if (value != characterFilter)
+                {
+                    characterFilter = value;
+                    ActualCharacters?.Refresh();
                 }
             }
         }
