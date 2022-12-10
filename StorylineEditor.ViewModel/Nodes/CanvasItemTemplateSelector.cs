354079bf097d3efad80d1b147175563543c24fa9
klauth86 <kehel@yandex.ru>
2022-12-10 18:46:18 +0100

    Dialog Player p5

---
+++ StorylineEditor.ViewModel/Nodes/CanvasItemTemplateSelector.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StorylineEditor.ViewModel.Nodes
+{
+    public class CanvasDataTemplateSelector: DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return (item is INodeVM)
+                ? Application.Current.FindResource(string.Format("{0}_ItemTemplate", item.GetType().Name)) as DataTemplate
+                : base.SelectTemplate(item, container);
+        }
+    }
+}
